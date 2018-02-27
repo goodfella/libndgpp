633fb3c6517b6bb087225a968951bc33e12b8290
Nick Guiffrida <goodfella005@gmail.com>
2018-02-27 03:29:22 -0800

    [utility] print_type improvements
    - The print_type helper function was missing a semi colon
    - Add print_types: This helper function supports varadic templates

--- include/libndgpp/utility.hpp
+++ include/libndgpp/utility.hpp
@@ -1,6 +1,8 @@
 #ifndef LIBNDGPP_UTILITY_HPP
 #define LIBNDGPP_UTILITY_HPP
 
+#include <tuple>
+
 namespace ndgpp
 {
     /// Causes a compiler error upon instantiation so the type can be examined
@@ -8,10 +10,16 @@ namespace ndgpp
     class print_type_t;
 
     /// Helper function to generate a print_type_t compilation error
-    template<class T>
+    template <class T>
     inline decltype(auto) print_type(T&& t)
     {
-        return print_type_t<T>{}
+        return print_type_t<T>{};
+    }
+
+    template <class ... Ts>
+    inline decltype(auto) print_types(Ts&& ... ts)
+    {
+        return std::make_tuple(print_type_t<Ts>{}...);
     }
 }
 
