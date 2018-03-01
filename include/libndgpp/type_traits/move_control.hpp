3524045c94a7a16022be00039324fc0d15d26f2b
Nick Guiffrida <goodfella005@gmail.com>
2018-02-28 21:56:04 -0800

    [type_traits] Add move_control

---
+++ include/libndgpp/type_traits/move_control.hpp
@@ -0,0 +1,34 @@
+#ifndef LIBNDGPP_MOVE_CONTROL_HPP
+#define LIBNDGPP_MOVE_CONTROL_HPP
+
+namespace ndgpp
+{
+    namespace detail
+    {
+        struct moveable_type {};
+        struct non_moveable_type
+        {
+            non_moveable_type() = default;
+            non_moveable_type(const non_moveable_type&) = delete;
+            non_moveable_type& operator=(const non_moveable_type&) = delete;
+        };
+    }
+
+    /** Conditionally defines a type that is either movable or non-movable
+     *
+     *  Defines a type that is both move constructible and move
+     *  assignable if all the specified types are both move
+     *  constructible and move assignable.
+
+     *  @tparam Ts The list of types whose move semantics dictate
+     *          whether or not move_control is move constructible
+     *          and move assignable.
+     */
+    template <class ... Ts>
+    using move_control = std::conditional_t<ndgpp::conjunction<std::is_move_constructible<Ts>...>::value &&
+                                            ndgpp::conjunction<std::is_move_assignable<Ts>...>::value,
+                                            ndgpp::detail::moveable_type,
+                                            ndgpp::detail::non_moveable_type>;
+}
+
+#endif
