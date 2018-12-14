c664f9da46e4b75ed8019d359ae0af448e289f4b
Nick Guiffrida <goodfella005@gmail.com>
2018-12-13 20:21:04 -0800

    Add port value type

---
+++ include/libndgpp/net/port.hpp
@@ -0,0 +1,24 @@
+#ifndef LIBNDGPP_NET_PORT_HPP
+#define LIBNDGPP_NET_PORT_HPP
+
+#include <cstdint>
+#include <limits>
+#include <libndgpp/bounded_integer.hpp>
+
+namespace ndgpp
+{
+namespace net
+{
+    namespace detail
+    {
+        struct port_tag;
+    }
+
+    using port = ndgpp::bounded_integer<uint16_t,
+                                        std::numeric_limits<uint16_t>::min(),
+                                        std::numeric_limits<uint16_t>::max(),
+                                        linuxpp::net::detail::port_tag>;
+}
+}
+
+#endif
