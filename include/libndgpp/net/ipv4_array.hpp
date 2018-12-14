a9243c1feaddb186b55c33c34fb14972c4a29d2e
Nick Guiffrida <goodfella005@gmail.com>
2018-12-13 20:18:06 -0800

    Add IPv4 address value type

---
+++ include/libndgpp/net/ipv4_array.hpp
@@ -0,0 +1,13 @@
+#ifndef LIBNDGPP_NET_IPV4_ARRAY_HPP
+#define LIBNDGPP_NET_IPV4_ARRAY_HPP
+
+#include <array>
+#include <cstdint>
+
+namespace ndgpp {
+namespace net {
+
+    using ipv4_array = std::array<uint8_t, 4>;
+}}
+
+#endif
