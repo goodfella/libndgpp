505c47c38fcf8837ee7dc497ea518222da6c10ee
Nick Guiffrida <goodfella005@gmail.com>
2018-12-20 10:35:27 -0800

    net/ipv4_address.hpp: Remove vestigial includes

--- include/libndgpp/net/ipv4_address.hpp
+++ include/libndgpp/net/ipv4_address.hpp
@@ -1,16 +1,6 @@
 #ifndef LIBNDGPP_NET_IPV4_ADDRESS_HPP
 #define LIBNDGPP_NET_IPV4_ADDRESS_HPP
 
-#include <cinttypes>
-#include <cstdint>
-#include <cstdio>
-
-#include <array>
-#include <ostream>
-#include <sstream>
-#include <string>
-#include <utility>
-
 #include <libndgpp/net/basic_ipv4_address.hpp>
 
 namespace ndgpp {
